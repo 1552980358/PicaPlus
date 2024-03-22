538f3d74f2d1d5252b2653d4443ea227035c9998
1552980358 <1552980358@qq.com>
2024-03-22 18:59:00 +0800

    PicaPlus: Add dependency OkHttp

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.squareup.okhttp)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
