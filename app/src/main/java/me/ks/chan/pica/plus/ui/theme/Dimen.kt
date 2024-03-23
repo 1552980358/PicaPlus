1cd641fb4cfede74673bf4b06fb24e64389862d3
1552980358 <1552980358@qq.com>
2024-03-23 18:20:22 +0800

    Dimen: Update 24dp Icon

---
+++ app/src/main/java/me/ks/chan/pica/plus/ui/theme/Dimen.kt
@@ -0,0 +1,10 @@
+package me.ks.chan.pica.plus.ui.theme
+
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+private val Dp_24 by lazy { 24.dp }
+
+val Icon_24: Dp
+    get() = Dp_24
+
