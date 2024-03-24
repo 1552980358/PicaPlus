7727b0210f83c8f2d1e84b73c161b9c985ceaf8a
1552980358 <1552980358@qq.com>
2024-03-25 00:37:56 +0800

    Resources: Update constant NullResources

---
+++ app/src/main/java/me/ks/chan/pica/plus/util/android/Resources.kt
@@ -0,0 +1,53 @@
+package me.ks.chan.pica.plus.util.android
+
+import androidx.annotation.AnimRes
+import androidx.annotation.AnimatorRes
+import androidx.annotation.AnyRes
+import androidx.annotation.ArrayRes
+import androidx.annotation.AttrRes
+import androidx.annotation.BoolRes
+import androidx.annotation.ColorRes
+import androidx.annotation.DimenRes
+import androidx.annotation.DrawableRes
+import androidx.annotation.FontRes
+import androidx.annotation.FractionRes
+import androidx.annotation.IdRes
+import androidx.annotation.IntegerRes
+import androidx.annotation.InterpolatorRes
+import androidx.annotation.LayoutRes
+import androidx.annotation.MenuRes
+import androidx.annotation.NavigationRes
+import androidx.annotation.PluralsRes
+import androidx.annotation.RawRes
+import androidx.annotation.StringRes
+import androidx.annotation.StyleRes
+import androidx.annotation.StyleableRes
+import androidx.annotation.TransitionRes
+import androidx.annotation.XmlRes
+import androidx.core.content.res.ResourcesCompat
+
+@AnimRes
+@AnimatorRes
+@AnyRes
+@ArrayRes
+@AttrRes
+@BoolRes
+@ColorRes
+@DimenRes
+@DrawableRes
+@FontRes
+@FractionRes
+@IdRes
+@IntegerRes
+@InterpolatorRes
+@LayoutRes
+@MenuRes
+@NavigationRes
+@PluralsRes
+@RawRes
+@StringRes
+@StyleRes
+@StyleableRes
+@TransitionRes
+@XmlRes
+const val NullResources = ResourcesCompat.ID_NULL
