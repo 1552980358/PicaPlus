4d7c060c1d9b6e73662d23e8b6fb096b4ad4b724
1552980358 <1552980358@qq.com>
2024-03-25 14:17:14 +0800

    Dimen: Update dimens

--- app/src/main/java/me/ks/chan/pica/plus/ui/theme/Dimen.kt
+++ app/src/main/java/me/ks/chan/pica/plus/ui/theme/Dimen.kt
@@ -3,8 +3,15 @@ package me.ks.chan.pica.plus.ui.theme
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
+private val Dp_8 by lazy { 8.dp }
+private val Dp_16 by lazy { 16.dp }
 private val Dp_24 by lazy { 24.dp }
 
 val Icon_24: Dp
     get() = Dp_24
 
+val Spacing_8: Dp
+    get() = Dp_8
+
+val Spacing_16: Dp
+    get() = Dp_16
