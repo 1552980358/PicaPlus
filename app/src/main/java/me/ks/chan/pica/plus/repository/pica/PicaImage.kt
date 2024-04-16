21b29c0bcb68e24b03ffd1d495ed0300e8a5f031
1552980358 <1552980358@qq.com>
2024-04-16 09:51:30 +0800

    PicaRepository: Update pica image model

---
+++ app/src/main/java/me/ks/chan/pica/plus/repository/pica/PicaImage.kt
@@ -0,0 +1,26 @@
+package me.ks.chan.pica.plus.repository.pica
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import java.io.File
+
+private const val FieldName = "originalName"
+private const val FieldHost = "fileServer"
+private const val FieldPath = "path"
+
+private const val UrlStaticRoute = "/static/"
+
+@Serializable
+data class PicaImage(
+    @SerialName(FieldName)
+    val name: String,
+    @SerialName(FieldHost)
+    val host: String,
+    @SerialName(FieldPath)
+    val path: String,
+) {
+
+    val url: String
+        get() = host + UrlStaticRoute + path
+
+}
