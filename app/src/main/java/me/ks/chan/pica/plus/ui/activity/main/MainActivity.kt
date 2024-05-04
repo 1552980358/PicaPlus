60d3d10902d205d4a5e9ff3c685f2cfc501f3ace
1552980358 <1552980358@qq.com>
2024-05-05 01:24:44 +0800

    Ui/Activity/Main: Update routes

--- app/src/main/java/me/ks/chan/pica/plus/ui/activity/main/MainActivity.kt
+++ app/src/main/java/me/ks/chan/pica/plus/ui/activity/main/MainActivity.kt
@@ -6,9 +6,27 @@ import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import me.ks.chan.pica.plus.repository.pica.PicaRepository
+import me.ks.chan.pica.plus.ui.activity.main.viewmodel.MainState
+import me.ks.chan.pica.plus.ui.nav.Greeting
+import me.ks.chan.pica.plus.ui.nav.Guest
+import me.ks.chan.pica.plus.ui.nav.guestNav
+import me.ks.chan.pica.plus.ui.nav.guestNavPreview
 import me.ks.chan.pica.plus.ui.theme.PicaPlusTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge(
             navigationBarStyle = SystemBarStyle.auto(
@@ -16,9 +34,66 @@ class MainActivity : ComponentActivity() {
             )
         )
         super.onCreate(savedInstanceState)
+
+        val splashScreen = installSplashScreen()
+        val viewModel: MainViewModel by viewModels()
+        splashScreen.setKeepOnScreenCondition {
+            viewModel.state.value !is MainState.Result
+        }
+
         setContent {
-            PicaPlusTheme {
+            val state by viewModel.state.collectAsStateWithLifecycle()
+
+            MainContent(
+                state = state,
+            )
+        }
+
+        viewModel.splashSetup(this)
+    }
+
+}
+
+@Composable
+private fun MainContent(
+    state: MainState,
+    navController: NavHostController = rememberNavController(),
+) {
+    LaunchedEffect(key1 = state) {
+        if (state is MainState.Token) {
+            PicaRepository.authorization(state.token)
+            if (PicaRepository.isAuthorized) {
+                navController.navigate(Greeting)
             }
         }
+    }
+
+    PicaPlusTheme {
+        MainContent(navController = navController) {
+            guestNav(navController)
+        }
+    }
+}
+
+@Composable
+private fun MainContent(
+    navController: NavHostController,
+    startDestination: String = Guest,
+    navGraphBuilder: NavGraphBuilder.() -> Unit,
+) {
+    NavHost(
+        navController = navController,
+        startDestination = startDestination,
+        builder = navGraphBuilder,
+    )
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun Preview(
+    navController: NavHostController = rememberNavController(),
+) {
+    MainContent(navController = navController) {
+        guestNavPreview(navController = navController)
     }
 }
