852cb17fd39625d2f559261c18a00ca44ef64db2
1552980358 <1552980358@qq.com>
2024-05-05 01:32:33 +0800

    PicaRepository: Fix invalid response body

--- app/src/main/java/me/ks/chan/pica/plus/repository/pica/users/profile/PicaProfileRepository.kt
+++ app/src/main/java/me/ks/chan/pica/plus/repository/pica/users/profile/PicaProfileRepository.kt
@@ -13,59 +13,67 @@ import me.ks.chan.pica.plus.repository.pica.users.profile.PicaProfileRepositoryR
 import me.ks.chan.pica.plus.repository.pica.users.profile.PicaProfileRepositoryResult.Success
 import me.ks.chan.pica.plus.util.okhttp.RequestUnauthorized
 
-private const val FieldId = "_id"
-private const val FieldUsername = "email"
-private const val FieldAvatar = "avatar"
-private const val FieldNickname = "name"
-private const val FieldBirthday = "birthday"
-private const val FieldGender = "gender"
-private const val FieldSlogan = "slogan"
-private const val FieldExperience = "exp"
-private const val FieldLevel = "level"
-private const val FieldTitle = "title"
-private const val FieldCharacterList = "characters"
-private const val FieldPunched = "isPunched"
-private const val FieldRegister = "created_at"
-private const val FieldVerified = "verified"
+private const val FieldUser = "user"
+
+private const val UserFieldId = "_id"
+private const val UserFieldUsername = "email"
+private const val UserFieldAvatar = "avatar"
+private const val UserFieldNickname = "name"
+private const val UserFieldBirthday = "birthday"
+private const val UserFieldGender = "gender"
+private const val UserFieldSlogan = "slogan"
+private const val UserFieldExperience = "exp"
+private const val UserFieldLevel = "level"
+private const val UserFieldTitle = "title"
+private const val UserFieldCharacterList = "characters"
+private const val UserFieldPunched = "isPunched"
+private const val UserFieldRegister = "created_at"
+private const val UserFieldVerified = "verified"
 
 @Serializable
 private data class Data(
-    @SerialName(FieldId)
-    val id: String,
-    @SerialName(FieldUsername)
-    val username: String,
+    @SerialName(FieldUser)
+    val user: User
+) {
+    @Serializable
+    data class User(
+        @SerialName(UserFieldId)
+        val id: String,
+        @SerialName(UserFieldUsername)
+        val username: String,
 
-    @SerialName(FieldAvatar)
-    val avatar: PicaImage,
-    @SerialName(FieldNickname)
-    val nickname: String,
+        @SerialName(UserFieldAvatar)
+        val avatar: PicaImage,
+        @SerialName(UserFieldNickname)
+        val nickname: String,
 
-    @SerialName(FieldBirthday)
-    val birthday: String,
-    @SerialName(FieldGender)
-    val gender: Char,
+        @SerialName(UserFieldBirthday)
+        val birthday: String,
+        @SerialName(UserFieldGender)
+        val gender: Char,
 
-    @SerialName(FieldSlogan)
-    val slogan: String,
+        @SerialName(UserFieldSlogan)
+        val slogan: String,
 
-    @SerialName(FieldTitle)
-    val title: String,
-    @SerialName(FieldCharacterList)
-    val characterList: List<String>,
+        @SerialName(UserFieldTitle)
+        val title: String,
+        @SerialName(UserFieldCharacterList)
+        val characterList: List<String>,
 
-    @SerialName(FieldExperience)
-    val experience: Int,
-    @SerialName(FieldLevel)
-    val level: Int,
+        @SerialName(UserFieldExperience)
+        val experience: Int,
+        @SerialName(UserFieldLevel)
+        val level: Int,
 
-    @SerialName(FieldPunched)
-    val punched: Boolean,
+        @SerialName(UserFieldPunched)
+        val punched: Boolean,
 
-    @SerialName(FieldRegister)
-    val register: String,
-    @SerialName(FieldVerified)
-    val verified: Boolean,
-)
+        @SerialName(UserFieldRegister)
+        val register: String,
+        @SerialName(UserFieldVerified)
+        val verified: Boolean,
+    )
+}
 
 @Serializable
 private data class ResponseBody(
@@ -78,32 +86,14 @@ private const val ProfileApiPath = "users/profile"
 
 object PicaProfileRepository {
 
-    private fun asSuccessResult(data: Data): Success {
-        return Success(
-            id = data.id,
-            username = data.username,
-            avatar = data.avatar.url,
-            nickname = data.nickname,
-            birthday = data.birthday,
-            gender = data.gender,
-            slogan = data.slogan,
-            title = data.title,
-            characterList = data.characterList,
-            experience = data.experience,
-            level = data.level,
-            punched = data.punched,
-            register = data.register,
-            verified = data.verified,
-        )
-    }
-
     val repositoryFlow = flow {
         val response = PicaRepository.get(ProfileApiPath)
         val result = when (response.code) {
             RequestSuccess -> {
                 response.deserialize<ResponseBody>()
                     ?.data
-                    ?.let(::asSuccessResult)
+                    ?.user
+                    ?.let(::collectResultState)
             }
             RequestUnauthorized -> {
                 Error(Error.Type.Unauthorized)
@@ -119,4 +109,23 @@ object PicaProfileRepository {
         )
     }
 
+}
+
+private fun collectResultState(user: Data.User): Success {
+    return Success(
+        id = user.id,
+        username = user.username,
+        avatar = user.avatar.url,
+        nickname = user.nickname,
+        birthday = user.birthday,
+        gender = user.gender,
+        slogan = user.slogan,
+        title = user.title,
+        characterList = user.characterList,
+        experience = user.experience,
+        level = user.level,
+        punched = user.punched,
+        register = user.register,
+        verified = user.verified,
+    )
 }
