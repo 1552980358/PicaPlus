e59d94c46b13fa02ed910c6d27627aa51a3ddff8
1552980358 <1552980358@qq.com>
2024-05-17 19:11:14 +0800

    Ui/Theme: Disable system bar contrast

--- app/src/main/java/me/ks/chan/pica/plus/ui/theme/Theme.kt
+++ app/src/main/java/me/ks/chan/pica/plus/ui/theme/Theme.kt
@@ -1,16 +1,16 @@
 package me.ks.chan.pica.plus.ui.theme
 
 import android.app.Activity
+import android.os.Build
 import android.view.View
 import android.view.Window
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
@@ -48,9 +48,19 @@ private fun setupSystemBars(
     isDarkTheme: Boolean,
     window: Window = (view.context as Activity).window
 ) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        // Disable system bar contrast
+        disableSystemBarsContrast(window = window)
+    }
     with(WindowCompat.getInsetsController(window, view)) {
         // Appearance (icons, text)
         isAppearanceLightStatusBars = !isDarkTheme
         isAppearanceLightNavigationBars = !isDarkTheme
     }
+}
+
+@RequiresApi(Build.VERSION_CODES.Q)
+private fun disableSystemBarsContrast(window: Window) {
+    window.isStatusBarContrastEnforced = false
+    window.isNavigationBarContrastEnforced = false
 }
