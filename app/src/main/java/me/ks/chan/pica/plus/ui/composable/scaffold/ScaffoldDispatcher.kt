3efb6c2c4df4200d43d788474e31b459a62ebbdd
1552980358 <1552980358@qq.com>
2024-05-28 23:01:05 +0800

    Ui/Composable/Scaffold: Update TopBarActions to dispatcher

--- app/src/main/java/me/ks/chan/pica/plus/ui/composable/scaffold/ScaffoldDispatcher.kt
+++ app/src/main/java/me/ks/chan/pica/plus/ui/composable/scaffold/ScaffoldDispatcher.kt
@@ -1,8 +1,12 @@
 package me.ks.chan.pica.plus.ui.composable.scaffold
 
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 
 val LocalScaffoldDispatcher = compositionLocalOf(
     defaultFactory = ::ScaffoldDispatcher
@@ -11,9 +15,15 @@ val LocalScaffoldDispatcher = compositionLocalOf(
 class ScaffoldDispatcher {
 
     private var navBarItemSecondaryClick: (() -> Unit)? = null
+    private var topAppBarActions by mutableStateOf<(@Composable RowScope.() -> Unit)?>(null)
 
     fun onNavBarItemSecondaryClick() {
         navBarItemSecondaryClick?.invoke()
+    }
+
+    @Composable
+    fun TopAppBarActions(rowScope: RowScope) {
+        topAppBarActions?.invoke(rowScope)
     }
 
     @Composable
@@ -28,10 +38,28 @@ class ScaffoldDispatcher {
         }
     }
 
+    @Composable
+    fun TopAppBarActions(actions: @Composable RowScope.() -> Unit) {
+        DisposableEffect(key1 = Unit) {
+            topAppBarActions = actions
+            onDispose {
+                if (topAppBarActions == actions) {
+                    topAppBarActions = null
+                }
+            }
+        }
+    }
+
 }
 
 @Composable
 fun ProvideNavBarItemSecondaryClick(action: () -> Unit) {
     LocalScaffoldDispatcher.current
         .NavBarItemSecondaryClick(action)
+}
+
+@Composable
+fun ProvideTopAppBarActions(actions: @Composable RowScope.() -> Unit) {
+    LocalScaffoldDispatcher.current
+        .TopAppBarActions(actions)
 }
