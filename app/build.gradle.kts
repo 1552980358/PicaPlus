de22d991c1c83a46fe4e486e1dd00190c1e85510
1552980358 <1552980358@qq.com>
2024-05-28 23:07:37 +0800

    PicaPlus: Update kotlinx datetime library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -103,6 +103,7 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.google.protobuf.kotlin.lite)
     implementation(libs.jetbrains.kotlin.reflect)
+    implementation(libs.jetbrains.kotlinx.datetime)
     implementation(libs.jetbrains.kotlinx.serialization.json)
     implementation(libs.squareup.okhttp)
     implementation(libs.io.coil.compose)
