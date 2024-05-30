fde5adc03467f81660e2b94866b03f6d7e923047
1552980358 <1552980358@qq.com>
2024-05-30 18:55:36 +0800

    Util/OkHttp: Update extension method Response.deserialize()

--- app/src/main/java/me/ks/chan/pica/plus/util/okhttp/Response.kt
+++ app/src/main/java/me/ks/chan/pica/plus/util/okhttp/Response.kt
@@ -7,3 +7,15 @@ inline fun <reified T> Response.deserialize(): T? {
     return body?.string()
         ?.let(Json::decodeFromString)
 }
+
+inline fun <reified T, reified R> Response.deserialize(
+    crossinline catchBody: (body: String) -> Unit = {},
+    crossinline transform: (T.() -> R) = { this as R },
+): R? {
+    return body?.string()
+        ?.let { body ->
+            runCatching { Json.decodeFromString<T>(body).transform() }
+                .onFailure { body.let(catchBody) }
+                .getOrThrow()
+        }
+}
