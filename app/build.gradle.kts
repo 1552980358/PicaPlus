880f44781e407b76f5411e52d6ff4ebb0c4daeec
1552980358 <1552980358@qq.com>
2024-08-17 23:48:42 +0800

    [Gradle] Apply dependency bundles

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,30 +63,14 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
-    implementation(libs.jetbrains.kotlinx.coroutines.android)
-    implementation(libs.jetbrains.kotlinx.serialization)
-    implementation(libs.material.symbols.compose.annotation)
+    api(libs.bundles.app)
     ksp(libs.material.symbols.compose.ksp)
-    api(libs.squareup.okhttp)
-    implementation(libs.squareup.retrofit)
-    implementation(libs.squareup.retrofit.converter.kotlinx.serialization)
 
-    testImplementation(libs.junit)
-    testImplementation(libs.jetbrains.kotlinx.coroutines.test)
+    testImplementation(libs.bundles.test)
 
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
+    androidTestImplementation(libs.bundles.android.test)
 
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+    debugImplementation(libs.bundles.debug)
 }
